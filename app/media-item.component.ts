e39cd3e358b0e048819ec885703e7874841b4e83
yaoharry <harry@harryyao.com>
2018-07-12 18:57:43 -0700

    episode 2 edits

--- app/media-item.component.ts
+++ app/media-item.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'mw-media-item',
   templateUrl: 'app/media-item.component.html',
   styleUrls: ['app/media-item.component.css']
 })
-export class MediaItemComponent { }
+export class MediaItemComponent {
+  @Input() mediaItem;
+
+  onDelete() {
+    console.log('deleted');
+  }
+}
